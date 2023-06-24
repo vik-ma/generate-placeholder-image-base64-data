594ed1723c39faceb826a2fd8a5cf656867ece9b
Viktor <vikt.mag@gmail.com>
2023-06-24 22:26:26 +0200

    Made script work with multiple files in one folder

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 import base64
 from io import BytesIO
 from PIL import Image
+import os
 
 # CUSTOMIZE THESE VALUES
-OUTPUT_WIDTH = 8
-OUTPUT_HEIGHT = 5
-OUTPUT_QUALITY = 100
+OUTPUT_IMAGE_WIDTH = 8
+OUTPUT_IMAGE_HEIGHT = 5
+OUTPUT_IMAGE_QUALITY = 100
+INPUT_FOLDER_DIR = "input"
 
-file_name = r"test.png"
 
-img_name = file_name.rsplit(".", 1)[0]
+def generate_base64_strings(input_files):
+    output_text = []
 
-img = Image.open(file_name)
+    for file in input_files:
+        if not (file.endswith(".png") or file.endswith(".jpg")\
+                or file.endswith(".jpeg") or file.endswith(".gif")):
+            print(f"Did not load {file} because it is not a valid type!")
+            continue
+        
+        img_name = file.rsplit(".", 1)[0]
 
-resized_img = img.resize((OUTPUT_WIDTH, OUTPUT_HEIGHT), Image.LANCZOS)
+        img = Image.open(f"{INPUT_FOLDER_DIR}/{file}")
 
-# resized_img.save("output.jpg", optimize=True, quality=OUTPUT_QUALITY)
+        resized_img = img.resize((OUTPUT_IMAGE_WIDTH, OUTPUT_IMAGE_HEIGHT), Image.LANCZOS)
 
-buffered = BytesIO()
-resized_img.save(buffered, optimize=True, quality=OUTPUT_QUALITY, format="JPEG")
-base64_data = base64.b64encode(buffered.getvalue())
+        # resized_img.save("output.jpg", optimize=True, quality=OUTPUT_QUALITY)
 
-image_data_uri = "data:image/jpeg;base64," + str(base64_data)[2:-1]
+        buffered = BytesIO()
+        resized_img.save(buffered, optimize=True, quality=OUTPUT_IMAGE_QUALITY, format="JPEG")
+        base64_data = base64.b64encode(buffered.getvalue())
 
-output_text = [img_name, image_data_uri]
+        image_data_uri = "data:image/jpeg;base64," + str(base64_data)[2:-1]
 
-with open("output.txt", "w") as file:
-    file.write('\n'.join(output_text))
-    file.write('\n')
+        output_text.extend([img_name, '\n', image_data_uri, '\n', '\n'])
+
+    if output_text != []:
+        with open("output.txt", "w") as file:
+            file.writelines(output_text)
+
+        print("Finished")
+    else:
+        print(f"No valid files in '{INPUT_FOLDER_DIR}' folder!")
+
+if __name__ == "__main__":
+    if not os.path.exists(INPUT_FOLDER_DIR):
+        print(f"No folder named '{INPUT_FOLDER_DIR}'!\nCreate a folder in this directory with the name '{INPUT_FOLDER_DIR}' and put your images inside there.")
+    else:
+        input_files = os.listdir(INPUT_FOLDER_DIR)
+
+        if len(input_files) > 0:
+            generate_base64_strings(input_files)
+        else:
+            print(f"No files in '{INPUT_FOLDER_DIR}' folder!")
